99fae2d375e9ec039e256cf41132fb7da74be71a
durcanm <mdurcan@gmail.com>
2019-11-24 23:08:04 +0300

    minor formatting

--- relay/relay-test.ino
+++ relay/relay-test.ino
@@ -11,7 +11,7 @@ void setup()
   pinMode(pin_relay3, OUTPUT);
   pinMode(pin_relay4, OUTPUT);
   pinMode(pin_relay5, OUTPUT);
-  
+
   digitalWrite(pin_relay1, LOW);
   digitalWrite(pin_relay2, LOW);
   digitalWrite(pin_relay3, LOW);
@@ -22,40 +22,39 @@ void setup()
 void loop()
 {
   int _delay = 3000;
-  
+
   delay(100);
-  
+
   digitalWrite(pin_relay1, HIGH); delay(_delay);
   digitalWrite(pin_relay1, LOW); delay(_delay);  
-  
+
   digitalWrite(pin_relay2, HIGH); delay(_delay);
   digitalWrite(pin_relay2, LOW); delay(_delay);
-  
+
   digitalWrite(pin_relay3, HIGH); delay(_delay);
   digitalWrite(pin_relay3, LOW); delay(_delay);
-  
+
   digitalWrite(pin_relay4, HIGH); delay(_delay);
   digitalWrite(pin_relay4, LOW); delay(_delay);
-  
+
   digitalWrite(pin_relay5, HIGH); delay(_delay);
   digitalWrite(pin_relay5, LOW); delay(_delay);
-  
+
   delay(_delay);
-  
+
   digitalWrite(pin_relay1, HIGH);
   digitalWrite(pin_relay2, HIGH);
   digitalWrite(pin_relay3, HIGH);
   digitalWrite(pin_relay4, HIGH);
   digitalWrite(pin_relay5, HIGH);
-  
+
   delay(_delay);
-  
+
   digitalWrite(pin_relay1, LOW);
   digitalWrite(pin_relay2, LOW);
   digitalWrite(pin_relay3, LOW);
   digitalWrite(pin_relay4, LOW);
   digitalWrite(pin_relay5, LOW);
-  
-  delay(_delay);
-  
+
+  delay(_delay);  
 }
